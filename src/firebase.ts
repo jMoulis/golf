37a2f765c140310cc17dc9b60685e4bba138078d
Julien moulis <julien.moulis@moulis.me>
2022-05-29 19:18:43 +0200

    t

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVzdnw1cNgCo5HHBHHPOClEz0nSR5k0wY",
+  authDomain: "golf-67f27.firebaseapp.com",
+  databaseURL:
+    "https://golf-67f27-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "golf-67f27",
+  storageBucket: "golf-67f27.appspot.com",
+  messagingSenderId: "37704039951",
+  appId: "1:37704039951:web:27f7862c71d9612fd4ed7a"
+};
+
+initializeApp(firebaseConfig);
